42f7f18ca3518de267badac1c8a6df4fe642aade
vzaporozkyi <zaporozhskiy2002@gmail.com>
2024-05-22 19:19:12 +0300

    delete console.log in cart page + update create drug modal

--- client/src/components/modals/CreateDrug.jsx
+++ client/src/components/modals/CreateDrug.jsx
@@ -2,40 +2,158 @@ import Header from "../../components/header/Header";
 import { observer } from "mobx-react-lite";
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../../store";
-import { Button, Container, Form, Modal } from "react-bootstrap";
+import {
+   Button,
+   Col,
+   Container,
+   Dropdown,
+   Form,
+   Modal,
+   Row,
+} from "react-bootstrap";
 
 const CreateDrug = ({ show, onHide }) => {
-   const [value, setValue] = useState("");
+   const { device } = useContext(Context);
+   const [name, setName] = useState("");
+   const [price, setPrice] = useState(0);
+   const [file, setFile] = useState(null);
+   const [info, setInfo] = useState([]);
 
-   const addDrug = () => {
-      // createDrug({ name: value }).then((data) => {
-      //    setValue("");
-      //    onHide();
-      // });
+   useEffect(() => {
+      // fetchTypes().then((data) => device.setTypes(data));
+      // fetchBrands().then((data) => device.setBrands(data));
+   }, []);
+
+   const addInfo = () => {
+      setInfo([...info, { title: "", description: "", number: Date.now() }]);
+   };
+   const removeInfo = (number) => {
+      setInfo(info.filter((i) => i.number !== number));
+   };
+   const changeInfo = (key, value, number) => {
+      setInfo(
+         info.map((i) => (i.number === number ? { ...i, [key]: value } : i))
+      );
+   };
+
+   const selectFile = (e) => {
+      setFile(e.target.files[0]);
+   };
+
+   const addDevice = () => {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("price", `${price}`);
+      formData.append("img", file);
+      // formData.append("brandId", device.selectedBrand.id);
+      // formData.append("typeId", device.selectedType.id);
+      formData.append("info", JSON.stringify(info));
+      // createDevice(formData).then((data) => onHide());
    };
 
    return (
       <Modal show={show} onHide={onHide} centered>
          <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-vcenter">
-               Добавить тип
+               Add medicine
             </Modal.Title>
          </Modal.Header>
          <Modal.Body>
             <Form>
+               <Dropdown className="mt-2 mb-2">
+                  <Dropdown.Toggle>
+                     {/* {device.selectedType.name || "Choose type"} */}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                     {/* {device.types.map((type) => (
+                        <Dropdown.Item
+                           // onClick={() => device.setSelectedType(type)}
+                           key={type.id}
+                        >
+                           {type.name}
+                        </Dropdown.Item>
+                     ))} */}
+                  </Dropdown.Menu>
+               </Dropdown>
+               <Dropdown className="mt-2 mb-2">
+                  <Dropdown.Toggle>
+                     {/* {device.selectedBrand.name || "Choose type"} */}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                     {/* {device.brands.map((brand) => (
+                        <Dropdown.Item
+                           onClick={() => device.setSelectedBrand(brand)}
+                           key={brand.id}
+                        >
+                           {brand.name}
+                        </Dropdown.Item>
+                     ))} */}
+                  </Dropdown.Menu>
+               </Dropdown>
                <Form.Control
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-                  placeholder={"Введите название типа"}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="mt-3"
+                  placeholder="Enter medicine name"
                />
+               <Form.Control
+                  value={price}
+                  onChange={(e) => setPrice(Number(e.target.value))}
+                  className="mt-3"
+                  placeholder="Enter the cost of the medicine"
+                  type="number"
+               />
+               <Form.Control
+                  className="mt-3"
+                  type="file"
+                  onChange={selectFile}
+               />
+               <hr />
+               <Button variant={"outline-dark"} onClick={addInfo}>
+                  Add new property
+               </Button>
+               {info.map((i) => (
+                  <Row className="mt-4" key={i.number}>
+                     <Col md={4}>
+                        <Form.Control
+                           value={i.title}
+                           onChange={(e) =>
+                              changeInfo("title", e.target.value, i.number)
+                           }
+                           placeholder="Enter a property name"
+                        />
+                     </Col>
+                     <Col md={4}>
+                        <Form.Control
+                           value={i.description}
+                           onChange={(e) =>
+                              changeInfo(
+                                 "description",
+                                 e.target.value,
+                                 i.number
+                              )
+                           }
+                           placeholder="Enter a property description"
+                        />
+                     </Col>
+                     <Col md={4}>
+                        <Button
+                           onClick={() => removeInfo(i.number)}
+                           variant={"outline-danger"}
+                        >
+                           Delete
+                        </Button>
+                     </Col>
+                  </Row>
+               ))}
             </Form>
          </Modal.Body>
          <Modal.Footer>
             <Button variant="outline-danger" onClick={onHide}>
-               Закрыть
+               Close
             </Button>
-            <Button variant="outline-success" onClick={addDrug}>
-               Добавить
+            <Button variant="outline-success" onClick={addDevice}>
+               Add
             </Button>
          </Modal.Footer>
       </Modal>
--- client/src/pages/Cart/CartPage.jsx
+++ client/src/pages/Cart/CartPage.jsx
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router-dom";
 
 const CartPage = observer(() => {
    const { pagesStore, userStore } = useContext(Context);
-   console.log(userStore.isAuth);
 
    if (!userStore.isAuth) {
       const navigate = useNavigate();
